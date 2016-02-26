11fce4c67a484d72f9986b11076ca137e993df3b
Luke Mclaren <luke.patrick.mclaren@gmail.com>
2016-02-25 18:13:21 -0800

    Final copy of classifier code

--- clf.py
+++ clf.py
@@ -4,106 +4,84 @@ from sklearn import cross_validation
 
 from sklearn import svm
 from sklearn import tree
+from sklearn import linear_model
+from sklearn.kernel_ridge import KernelRidge
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
 
-def parse_args(argv):
 
-	try:
-		opts, args = getopt.getopt(argv,"hov",["train=","test=","class=","attributes=","classifier="])
-	except getopt.GetoptError:
-		print 'clf.py --train <train file> --test <test_file> --attributes <example,example2,example3> --classifier <classifier> --class <predict class>'
-		print '-o argument'
-		print '\t\tOutputs csv files'
-		print '-v argument'
-		print '\t\tOutputs the cross validation score'
-		sys.exit(2)
 
-	output 	= False
-	cv 		= False
+'''
+Main function
+'''
+class Classifier():
+	def __init__(self, testfile, trainfile, predict, attributes, classifier, cv=False, output=False):
+		self.output 		= output
+		self.cv 			= cv 
+		self.testfile 		= testfile
+		self.trainfile  	= trainfile
+		self.predict		= predict
+		self.attributes 	= attributes 
+		self.clf 			= self.get_classifier(classifier)
 
-	for opt, arg in opts:
-		if opt == '-h':
-			print 'clf.py --train <train file> --test <test_file> --attributes <example,example2,example3> --classifier <classifier> --class <predict class>'
-			sys.exit()
-		elif opt in ("--test"):
-			testfile = arg
-		elif opt in ("--train"):
-			trainfile = arg
-		elif opt in ("--class"):
-			predict_class = arg
-		elif opt in ("--attributes"):
-			attribute_list = arg.split(',')
-		elif opt in ("-o"):
-			output = True
-		elif opt in ("-v"):
-			cv = True
-		elif opt in ("--classifier"):
-			if arg == 'gnb':
-				clf = GaussianNB()
-			elif arg == 'bnb':
-				clf = BernoulliNB()
-			elif arg == 'mnb':
-				clf = MultinomialNB()
-			elif arg == 'svm':
-				clf = svm.SVC()
-			elif arg == 'rf':
-				clf = RandomForestClassifier(max_depth=len(attribute_list), n_estimators=5, max_features=len(attribute_list))
-			elif arg == 'dt':
-				clf = tree.DecisionTreeClassifier()
+	def get_classifier(self, classifier):
+		if classifier == 'gnb':
+			return GaussianNB()
+		elif classifier == 'bnb':
+			return BernoulliNB()
+		elif classifier == 'mnb':
+			return MultinomialNB()
+		elif classifier == 'svm':
+			return svm.SVC(decision_function_shape='ovr')
+		elif classifier == 'rf':
+			return RandomForestClassifier(max_depth=len(self.attributes), n_estimators=len(self.attributes), max_features=len(self.attributes))
+		elif classifier == 'dt':
+			return tree.DecisionTreeClassifier()
+		elif classifier == 'ls':
+			return linear_model.LinearRegression()
+		elif classifier == 'pr':
+			return linear_model.Perceptron(fit_intercept=False, n_iter=10, shuffle=False)
+		elif classifier == 'kr':
+			return KernelRidge(alpha=1.0)
 
-	main(output, cv, testfile, trainfile, predict_class, clf, attribute_list)
+	def cross_validate(self):
+		parser_train 	= Parser(self.trainfile)
+		data_instances 	= parser_train.get_data(self.attributes)
+		classification 	= parser_train.get_class(self.predict)
 
-def main(output, cv, testfile, trainfile, predict_class, clf, attribute_list):
+		data_train, data_test, target_train, target_test = self.c_v(data_instances, classification)
+		clf = self.clf.fit(data_train, target_train)
 
-	parser_train 	= Parser(trainfile)
-	data_instances 	= parser_train.get_data(attribute_list)
-	classification 	= parser_train.get_class(predict_class)
-	
-	
-	if cv:
-		data_train, data_test, target_train, target_test = cross_validate(data_instances, classification)
-		clf = clf.fit(data_train, target_train)
+		return clf.score(data_test, target_test)
 
-		print "Score for " + str(clf) + " classifier:"
-		print clf.score(data_test, target_test)
-	else:
-		clf = clf.fit(data_instances, classification)
+	def c_v(self, data, target):
 
-		parser_test = Parser(testfile)
+		data_train, data_test, target_train, target_test = cross_validation.train_test_split(
+			data, target, test_size=0.4, random_state=0
+		)
 
-		test_data	= parser_test.get_data(attribute_list)
-		test_class	= parser_test.get_class(predict_class)
+		return data_train, data_test, target_train, target_test
+
+	def output_csv(self):
+		parser_train 	= Parser(self.trainfile)
+		data_instances 	= parser_train.get_data(self.attributes)
+		classification 	= parser_train.get_class(self.predict)
+		clf 			= self.clf.fit(data_instances, classification)
+
+		parser_test = Parser(self.testfile)
+
+		test_data	= parser_test.get_data(self.attributes)
+		test_class	= parser_test.get_class(self.predict)
 		test_id		= parser_test.get_class("id")
 
-		if output:
-			write_csv(parser_test, parser_train, clf, predict_class, test_data)
+		self.write_csv(parser_test, parser_train, clf, self.predict, test_data)
 
-def write_csv(parser_test, parser_train, clf, predict_class, test_data):
-	test_id	= parser_test.get_class("id")
-	ids 	= parser_test.convert_id(test_id, 'id')
-	values 	= parser_train.convert_id(clf.predict(test_data), predict_class)
+	def write_csv(self, parser_test, parser_train, clf, predict_class, test_data):
+		test_id	= parser_test.get_class("id")
+		ids 	= parser_test.convert_id(test_id, 'id')
+		values 	= parser_train.convert_id(clf.predict(test_data), predict_class)
 
-	for index in range(len(values)):
-		values[index] = [ids[index], values[index]]
-	parser_train.output_csv('output.csv', ["id","country"], values)
+		for index in range(len(values)):
+			values[index] = [ids[index], values[index]]
+		parser_train.output_csv('output.csv', ["id","country"], values)
 
-def cross_validate(data, target):
-	data_train, data_test, target_train, target_test = cross_validation.train_test_split(
-		data, target, test_size=0.4, random_state=0
-	)
-	return data_train, data_test, target_train, target_test
-
-#output.csv file created here
-def ouput_csv(parser, clf):
-	
-	ids 	= parser_test.convert_id(test_id, 'id')
-	values 	= parser.convert_id(clf.predict(test_data), 'country_destination')
-	
-	for x in range(len(g_values)):
-		g_values[x] = [ids[x],g_values[x]]
-	
-	parser_train.output_csv('output.csv', ["id","country"], g_values)
-
-if __name__ == "__main__":
-   parse_args(sys.argv[1:])
